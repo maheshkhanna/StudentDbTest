ee53d0ad703beaefaba5c2572c7e9e8f2b7fd34b
mahesh-zt164 <mahesh-zt164@localhost>
2020-06-06 10:09:44 +0530

    Added sample project

---
+++ src/servlet/StudentServlet.java
@@ -0,0 +1,38 @@
+//$Id$
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class StudentServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	public static Map<String,String> studentDetails = new HashMap<String,String>();
+	
+	public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException
+	{
+		System.out.print(studentDetails);
+		String id = request.getParameter("id");
+		if(studentDetails.containsKey(id))
+		{
+			PrintWriter out = response.getWriter();
+			out.print(studentDetails.get(id).toString());
+			out.close();
+		}
+	}
+	
+	public void doPost(HttpServletRequest request,HttpServletResponse response)
+	{
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		studentDetails.put(id, name);
+		System.out.print(studentDetails);
+	}
+	
+}
